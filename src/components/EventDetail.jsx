b5d5c3e6e9a63b3a72c07500db7fbf004a206f79
Mahmut Emir <77195637+mahmutemirkr@users.noreply.github.com>
2024-03-03 21:25:32 +0300

    react-tailwind-css-v0.3.1

--- src/components/EventDetail.jsx
+++ src/components/EventDetail.jsx
@@ -1,4 +1,3 @@
-import { useParams } from 'react-router-dom';
 import { FaMapMarkerAlt } from 'react-icons/fa';
 
 const EventDetail = () => {
@@ -14,8 +13,6 @@ const EventDetail = () => {
     
   };
 
-
-  const { id } = useParams();
 
   const mapUrl = `https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6352252.721146542!2d29.84514769338753!3d38.9768396205632!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14b0155c964f2671%3A0x40d9dbd42a625f2a!2zVMO8cmtpeWU!5e0!3m2!1str!2str!4v1709489638291!5m2!1str!2str`;
 
